1f432ca9d588ac8a5effd0c5610b540c6674755e
HR0senberg <74983814+HR0senberg@users.noreply.github.com>
2024-02-03 22:39:53 +0300

    1

---
+++ index_htm_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_3_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/91.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/92.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/93.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/94.jpg"
+}
+];
---
+++ index_htm_files/2.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_0_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/97.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/98.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/99.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_htm_files/100.jpg"
+}
+];
