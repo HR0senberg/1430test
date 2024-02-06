3b5880bfb031f80a2387d1666fd8f48090d4dadc
HR0senberg <74983814+HR0senberg@users.noreply.github.com>
2024-02-06 11:05:55 +0300

    1

---
+++ contact_htm_files/2.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "contact_htm_files/114.jpg",
+ thumbnail: "contact_htm_files/132.jpg"
+},
+{
+ image: "contact_htm_files/116.jpg",
+ thumbnail: "contact_htm_files/133.jpg"
+},
+{
+ image: "contact_htm_files/118.jpg",
+ thumbnail: "contact_htm_files/134.jpg"
+},
+{
+ image: "contact_htm_files/120.jpg",
+ thumbnail: "contact_htm_files/135.jpg"
+},
+{
+ image: "contact_htm_files/122.jpg",
+ thumbnail: "contact_htm_files/136.jpg"
+},
+{
+ image: "contact_htm_files/124.jpg",
+ thumbnail: "contact_htm_files/137.jpg"
+},
+{
+ image: "contact_htm_files/126.jpg",
+ thumbnail: "contact_htm_files/138.jpg"
+},
+{
+ image: "contact_htm_files/128.jpg",
+ thumbnail: "contact_htm_files/139.jpg"
+}
+];
