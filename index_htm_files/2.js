2ff3e2f9259df42ac60c9a79413bad2226bb1b6f
HR0senberg <74983814+HR0senberg@users.noreply.github.com>
2024-02-08 11:04:01 +0300

    1

--- index_htm_files/2.js
+++ index_htm_files/2.js
@@ -4,23 +4,35 @@ window.local_simpleGallery3Columns_1_data = [
  showshadow: "Y"
 },
 {
- image: "index_htm_files/537.jpg",
- thumbnail: "index_htm_files/538.jpg"
+ image: "index_htm_files/116.jpg",
+ thumbnail: "index_htm_files/134.jpg"
 },
 {
- image: "index_htm_files/539.jpg",
- thumbnail: "index_htm_files/540.jpg"
+ image: "index_htm_files/118.jpg",
+ thumbnail: "index_htm_files/135.jpg"
 },
 {
- image: "index_htm_files/541.jpg",
- thumbnail: "index_htm_files/542.jpg"
+ image: "index_htm_files/120.jpg",
+ thumbnail: "index_htm_files/136.jpg"
 },
 {
- image: "index_htm_files/543.jpg",
- thumbnail: "index_htm_files/544.jpg"
+ image: "index_htm_files/122.jpg",
+ thumbnail: "index_htm_files/137.jpg"
 },
 {
- image: "index_htm_files/527.jpg",
- thumbnail: "index_htm_files/545.jpg"
+ image: "index_htm_files/124.jpg",
+ thumbnail: "index_htm_files/138.jpg"
+},
+{
+ image: "index_htm_files/126.jpg",
+ thumbnail: "index_htm_files/139.jpg"
+},
+{
+ image: "index_htm_files/128.jpg",
+ thumbnail: "index_htm_files/140.jpg"
+},
+{
+ image: "index_htm_files/130.jpg",
+ thumbnail: "index_htm_files/141.jpg"
 }
 ];
